4b97e3c9db5a9a6ba5fb66c2e7b7c131d0804163
derstolz <a>
2020-11-14 19:16:10 +0100

    add exception handling

--- util/bot_tools.py
+++ util/bot_tools.py
@@ -52,33 +52,37 @@ class VerbformenResponse:
 def parse_verben_page(html):
     soup = bs(html, 'html.parser')
     verbformen_response = VerbformenResponse()
-    for tag in soup.find_all('div', {'class': 'vTbl'}):
-        text = tag.text
-        for key in konjugation.keys():
-            if key.lower() in text.lower() and not konjugation[key]:
-                fixed_text = text \
-                    .replace(key, '') \
-                    .replace('\n', ' ') \
-                    .strip()
-                if any(f" {pr} " in fixed_text for pr in pronounces):
-                    for pr in pronounces[1:]:
-                        fixed_text = fixed_text \
-                            .replace(f" {pr} ", f"\n{pr} ")
-                else:
-                    fixed_text = fixed_text.replace(' ', '\n')
-                konjugation[key].append(fixed_text)
+    try:
+        for tag in soup.find_all('div', {'class': 'vTbl'}):
+            text = tag.text
+            for key in konjugation.keys():
+                if key.lower() in text.lower() and not konjugation[key]:
+                    fixed_text = text \
+                        .replace(key, '') \
+                        .replace('\n', ' ') \
+                        .strip()
+                    if any(f" {pr} " in fixed_text for pr in pronounces):
+                        for pr in pronounces[1:]:
+                            fixed_text = fixed_text \
+                                .replace(f" {pr} ", f"\n{pr} ")
+                    else:
+                        fixed_text = fixed_text.replace(' ', '\n')
+                    konjugation[key].append(fixed_text)
 
-                verbformen_response.konjugation[key] = f'\n{fixed_text}'
-
-    for lang in translations:
-        words = set()
-        for tag in soup.find('div', {'lang': lang}):
-            if type(tag) == Tag:
-                text = tag.text.strip()
-                if text and len(text) > 2:
-                    words.add(text)
-        verbformen_response.transactions[lang] = words
-
+                    verbformen_response.konjugation[key] = f'\n{fixed_text}'
+    except Exception as e:
+        print(e)
+    try:
+        for lang in translations:
+            words = set()
+            for tag in soup.find('div', {'lang': lang}):
+                if type(tag) == Tag:
+                    text = tag.text.strip()
+                    if text and len(text) > 2:
+                        words.add(text)
+            verbformen_response.transactions[lang] = words
+    except Exception as e:
+        print(e)
     for v in konjugation.values():
         v.clear()
     return verbformen_response

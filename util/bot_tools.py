f9b65fe4a64ea929596203f2dbee21663438dba2
derstolz <a>
2020-11-16 23:42:18 +0100

    add the support for nouns and adjectives in the searching service

--- util/bot_tools.py
+++ util/bot_tools.py
@@ -2,8 +2,9 @@
 import requests
 from bs4 import BeautifulSoup as bs
 from bs4.element import Tag
+import re
 
-BASE_URL = 'https://www.verbformen.de/konjugation'
+BASE_URL = 'https://www.verbformen.de'
 
 konjugation = {
     'Präsens': [],
@@ -30,13 +31,48 @@ pronounces = [
 ]
 
 
-class VerbformenResponse:
-    def __init__(self):
+class VerbResponse:
+    def __init__(self, html):
         self.konjugation = {}
-        self.transactions = {}
+        self.translations = {}
+
+        soup = bs(html, 'html.parser')
+        try:
+            for tag in soup.find_all('div', {'class': 'vTbl'}):
+                text = tag.text
+                for key in konjugation.keys():
+                    if key.lower() in text.lower() and not konjugation[key]:
+                        fixed_text = text \
+                            .replace(key, '') \
+                            .replace('\n', ' ') \
+                            .strip()
+                        if any(f" {pr} " in fixed_text for pr in pronounces):
+                            for pr in pronounces[1:]:
+                                fixed_text = fixed_text \
+                                    .replace(f" {pr} ", f"\n{pr} ")
+                        else:
+                            fixed_text = fixed_text.replace(' ', '\n')
+                        konjugation[key].append(fixed_text)
+
+                        self.konjugation[key] = f'\n{fixed_text}'
+        except Exception as e:
+            print(e)
+        try:
+            for lang in translations:
+                words = set()
+                for tag in soup.find('div', {'lang': lang}):
+                    if type(tag) == Tag:
+                        text = tag.text.strip()
+                        if text and len(text) > 2:
+                            words.add(text)
+                self.translations[lang] = words
+        except Exception as e:
+            print(e)
+        for v in konjugation.values():
+            v.clear()
 
     def is_empty(self):
-        return len(self.konjugation) == 0 or len(self.transactions) == 0
+        return len(self.konjugation) == 0
 
     def to_string(self):
         result = "Einfache Verbformen:\n"
@@ -44,54 +80,85 @@ class VerbformenResponse:
             result += f"*{k}*:{v}\n"
             result += '-' * 30 + '\n'
         result += "Übersetzungen:\n"
-        for k, v in self.transactions.items():
+        for k, v in self.translations.items():
             result += f"*{k}*: {', '.join(v)}\n"
         return result
 
 
-def parse_verben_page(html):
-    soup = bs(html, 'html.parser')
-    verbformen_response = VerbformenResponse()
-    try:
-        for tag in soup.find_all('div', {'class': 'vTbl'}):
-            text = tag.text
-            for key in konjugation.keys():
-                if key.lower() in text.lower() and not konjugation[key]:
-                    fixed_text = text \
-                        .replace(key, '') \
-                        .replace('\n', ' ') \
-                        .strip()
-                    if any(f" {pr} " in fixed_text for pr in pronounces):
-                        for pr in pronounces[1:]:
-                            fixed_text = fixed_text \
-                                .replace(f" {pr} ", f"\n{pr} ")
-                    else:
-                        fixed_text = fixed_text.replace(' ', '\n')
-                    konjugation[key].append(fixed_text)
+def remove_html_tags(word_with_tags):
+    tags = ['<b>', '</b>', '<i>', '</i>', '<u>', '</u>', '<wbr/>']
+    for t in tags:
+        word_with_tags = word_with_tags.replace(t, '')
+    return word_with_tags.strip()
 
-                    verbformen_response.konjugation[key] = f'\n{fixed_text}'
-    except Exception as e:
-        print(e)
-    try:
-        for lang in translations:
-            words = set()
-            for tag in soup.find('div', {'lang': lang}):
-                if type(tag) == Tag:
-                    text = tag.text.strip()
-                    if text and len(text) > 2:
-                        words.add(text)
-            verbformen_response.transactions[lang] = words
-    except Exception as e:
-        print(e)
-    for v in konjugation.values():
-        v.clear()
-    return verbformen_response
+class DeklinationResponse:
+    def __init__(self, html):
+        self.translations = {}
+        soup = bs(html, 'html.parser')
+        word_tag = soup.find('p', {'class': 'vGrnd rCntr'})
+        self.word = word_tag.text.strip()
+
+        word_forms_tag = soup.find('p', {'class': 'vStm rCntr'})
+        self.word_forms = [word.strip() for word in word_forms_tag.text.split('\n') if
+                           word.strip()
+                           and word != '.'
+                           and word != '·']
+
+        deklination_tag = soup.find('section', {'class': 'rBox rBoxTrns'})
+        self.deklination = re.sub(r"\n{2,}", "\n\n", deklination_tag.text)
+
+        try:
+            for lang in translations:
+                words = set()
+                for tag in soup.find('div', {'lang': lang}):
+                    if type(tag) == Tag:
+                        text = tag.text.strip()
+                        if text and len(text) > 2:
+                            words.add(text)
+                self.translations[lang] = words
+        except:
+            pass
+
+    def is_empty(self):
+        return not self.word and not self.word_forms
+
+    def to_string(self):
+        result = f"*{self.word}*\n_"
+        for word in self.word_forms:
+            result += f"{word}; "
+        result = result.rstrip("; ")
+        result += '_'
+
+        deklination = ''
+        keywords = ['Nom.', 'Gen.', 'Dat.', 'Akk.', 'Positiv', 'Komparativ', 'Superlativ']
+        for line in self.deklination.split('\n'):
+            if ',' in line:
+                deklination += f'*{line}*\n'
+                deklination += '-' * 20
+            elif any(k in line for k in keywords):
+                deklination += '-' * 20 + '\n'
+                deklination += f"_{line}_"
+            else:
+                deklination += line
+            deklination += '\n'
+
+        result += f'\n{deklination}'
+        return result
 
 
 def lookup_verbformen(query):
     url = f"{BASE_URL}/?w={query}"
-    resp = requests.get(url)
+    resp = requests.get(url, headers={
+        'User-Agent': 'Your Mom'
+    })
     if resp.ok:
-        return parse_verben_page(resp.text)
+        html = resp.text
+        redirect_url = resp.request.url
+        if 'konjugation' in redirect_url:
+            return VerbResponse(html)
+        elif 'deklination' in redirect_url:
+            return DeklinationResponse(html)
+        else:
+            print(f'Unsupported redirect URL: {redirect_url}')
     else:
         print(resp.status_code)
